49405cc37cb1ada5c45b67ab363fb977e490b696
oxavelar <omar.avelar@gmail.com>
2018-09-09 12:03:47 -0500

    V20: Remove the second screen builds

--- projects/V20-Kernel-Gamma/variants_build.sh
+++ projects/V20-Kernel-Gamma/variants_build.sh
@@ -9,16 +9,9 @@ variants="us996 vs995 h910 h918 h990 ls997"
 
 for variant in $variants
 do
-    # Single Screen
     rm -vrf "${CURR_PATH}/out/kbuild/arch/arm64/boot"
     time make -j10 KDEFCONFIG=lineageos_${variant}_oxavelar_defconfig
     mkdir -vp "${CURR_PATH}/release/${variant}"
     mv -v "${CURR_PATH}/out/Image.gz-dtb" "${CURR_PATH}/release/${variant}/Image.gz-dtb"
-    
-    # Second Screen
-    rm -vrf "${CURR_PATH}/out/kbuild/arch/arm64/boot"
-    time make -j10 KDEFCONFIG=lineageos_${variant}_oxavelar_defconfig CONFIG_LGE_DISABLE_SECOND_SCREEN=n
-    mkdir -vp "${CURR_PATH}/release/${variant}_ss"
-    mv -v "${CURR_PATH}/out/Image.gz-dtb" "${CURR_PATH}/release/${variant}_ss/Image.gz-dtb"
 done
 
